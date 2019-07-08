f2d915d6157c1b0a65fd275fcafbaa39ba3224fc
Yarrayya Naidu V <yarrayyanaidu.vobbalareddi-renexter@rntbci.com>
2019-07-08 21:49:14 +0530

    Made changes to convert jar package to war to deploy the package in external tomcat container.

--- src/main/java/io/javabrains/CourseApiDataApplication.java
+++ src/main/java/io/javabrains/CourseApiDataApplication.java
@@ -2,11 +2,18 @@ package io.javabrains;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CourseApiDataApplication {
+public class CourseApiDataApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApiDataApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CourseApiDataApplication.class);
+    }
 }
